731252c2c964783ddc672efc60fe13a366f6471f
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-28 02:45:22 -0500

    commit tree 3100137b1bb6051a20fffd94615a3558d4f7bb15

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+def sort_array_asc(array)
+    sorted_array = array
+    return sorted_array.sort
+end
 
+def sort_array_desc(array)
+    sorted_array = array
+    return sorted_array.sort { |a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+    sorted_array = array
+    return sorted_array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+    ashley = []
+    scott = []
+    ashley = array[1]
+    scott = array[2]
+    array[1] = scott
+    array[2] = ashley
+    return array
+end
+
+def reverse_array(array)
+    reversed_array = array
+    return reversed_array.reverse
+end
+
+def kesha_maker(array)
+    new_collection = []
+    array.each do |i|
+      new_collection << i[2] = "$"
+    end
+end
+
+def find_a (array)
+    array.select do |word|
+      word.chr == "a"
+    end
+  end
+
+
+def sum_array (array)
+    array.inject do |sum, num|
+      sum + num
+    end
+end
+
+def add_s (array)
+    array.collect.with_index do |word, index|
+        if index == 1
+            word
+        else
+            word + "s"
+        end
+    end
+end
